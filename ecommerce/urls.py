47ef959059a2f62c4ffe5b04293e5bfeb70f8d4e
bi-weleda <bilalw38@gmail.com>
2018-06-22 16:41:31 +0500

    images change reponsive design and other changes also done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^products/$', ProductsView.as_view(), name='product'),
-    url(r'^product_detail/$', ProductDetailView.as_view(), name='product'),
-    url(r'^contact/$', ContactView.as_view(), name='product'),
+    url(r'^product_detail/$', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^model/$', ModelView.as_view(), name='model')
 
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -11,13 +11,10 @@ from eCommerce import settings
 from django.shortcuts import render, redirect
 
 from ecommerce.forms import ContactForm
-from ecommerce.models import Slider,Product, SocailMedia
+from ecommerce.models import Slider, Product, SocailMedia
 
-
-
-from django.core.mail import send_mail
-
-
+# from django.core.mail import send_mail
+from django.core.mail import BadHeaderError, send_mail
 
 
 class IndexView(generic.TemplateView):
@@ -27,24 +24,52 @@ class IndexView(generic.TemplateView):
         slider_images = Slider.objects.all()
         products = Product.objects.all()
         social_media = SocailMedia.objects.all()
-        return render(request, self.template_name, {'slider_images': slider_images, 'products':products, 'social_media':social_media})
+        return render(request, self.template_name,
+                      {'slider_images': slider_images, 'products': products, 'social_media': social_media})
+
 
 class ProductsView(generic.TemplateView):
     template_name = 'product.html'
 
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
-        return render(request, self.template_name, {'products':products})
+        return render(request, self.template_name, {'products': products})
+
+    def post(self, request):
+        # import pdb;pdb.set_trace()
+        if request.method == 'GET':
+            form = ContactForm()
+        else:
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                subject = form.cleaned_data['subject']
+                from_email = form.cleaned_data['from_email']
+                msg = form.cleaned_data['message']
+                name = form.cleaned_data['name']
+                # message = 'This is message'
+                message = "Name: " + name + "\n" + "From: " + from_email + "\n" + "Subject: " + subject + "\n" + "Message: " + msg
+                try:
+                    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.EMAIL_HOST_USER, ],
+                              fail_silently=False, )
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+                return redirect('product')
+        # context = get_base_data()
+        # context['form'] = form
+        return render(request, "product.html", form)
 
 
 class ProductDetailView(generic.TemplateView):
     template_name = 'product-detail.html'
 
+
 class ContactView(generic.TemplateView):
     template_name = 'contact.html'
 
+
 class ModelView(generic.TemplateView):
     template_name = 'model.html'
+
 
 class ContactUsView(generic.TemplateView):
     template_name = 'ContactUs.html'
@@ -68,6 +93,23 @@ class ContactUsView(generic.TemplateView):
                 except BadHeaderError:
                     return HttpResponse('Invalid header found.')
                 return redirect('contactus')
-        # context = get_base_data()
-        # context['form'] = form
+
         return render(request, "contactUs.html", form)
+
+
+def send_email(request):
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('from_email', '')
+    name = request.POST.get('name', '')
+
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, name, ['bilalw38@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
